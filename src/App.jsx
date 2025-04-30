6ceb6054caa849c14175ba0410f7601855c57a0c
ian <leonpowell2013@gmail.com>
2025-04-30 09:09:53 +0100

    display question

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 
-// const questions =[
-//     {
-//       questionText: 'What is the capital of France?',
-//       answerOptions:[
-//         {answerText: 'New York', isCorrect: false},
-//         {answerText: 'London', isCorrect: false},
-//         {answerText: 'Paris', isCorrect: true},
-//         {answerText: 'Dublin', isCorrect: false},
-//       ],
-//     },
-//     {
-//       questionText: 'Who is Tesla CEO',
-//       answerOptions:[
-//         {answerText: 'Elon Musk', isCorrect: true},
-//         {answerText: 'Bill Gates', isCorrect: false},
-//         {answerText: 'Warren Buffett', isCorrect:false},
-//         {answerText: 'Jimmy Page', isCorrect: false},
-//       ],
-//     },
-//     {
-//       questionText: 'Who is Capital of India',
-//       answerOptions:[
-//         {answerText: 'Delhi', isCorrect: true},
-//         {answerText: 'Cairo', isCorrect: false},
-//         {answerText: 'Jakarta', isCorrect:false},
-//         {answerText: 'Mumbai', isCorrect: false},
-//       ],
-//     },
+const questions =[
+    {
+      questionText: 'What is the capital of France?',
+      answerOptions:[
+        {answerText: 'New York', isCorrect: false},
+        {answerText: 'London', isCorrect: false},
+        {answerText: 'Paris', isCorrect: true},
+        {answerText: 'Dublin', isCorrect: false},
+      ],
+    },
+    {
+      questionText: 'Who is Tesla CEO',
+      answerOptions:[
+        {answerText: 'Elon Musk', isCorrect: true},
+        {answerText: 'Bill Gates', isCorrect: false},
+        {answerText: 'Warren Buffett', isCorrect:false},
+        {answerText: 'Jimmy Page', isCorrect: false},
+      ],
+    },
+    {
+      questionText: 'Who is Capital of India',
+      answerOptions:[
+        {answerText: 'Delhi', isCorrect: true},
+        {answerText: 'Cairo', isCorrect: false},
+        {answerText: 'Jakarta', isCorrect:false},
+        {answerText: 'Mumbai', isCorrect: false},
+      ],
+    },
 
-// ];
+];
 
 
 function App() {
@@ -38,6 +38,9 @@ function App() {
         <div className="App">
           <div>
             <div>Quiz App</div>
+              <div>
+                <div>{questions[0].questionText}</div>
+              </div>
           </div>
         </div>
   )
