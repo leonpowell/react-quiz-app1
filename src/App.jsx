17bdf193ff5dec20cb242b2e5f0490e3f8ad53d1
ian <leonpowell2013@gmail.com>
2025-04-30 09:03:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+// const questions =[
+//     {
+//       questionText: 'What is the capital of France?',
+//       answerOptions:[
+//         {answerText: 'New York', isCorrect: false},
+//         {answerText: 'London', isCorrect: false},
+//         {answerText: 'Paris', isCorrect: true},
+//         {answerText: 'Dublin', isCorrect: false},
+//       ],
+//     },
+//     {
+//       questionText: 'Who is Tesla CEO',
+//       answerOptions:[
+//         {answerText: 'Elon Musk', isCorrect: true},
+//         {answerText: 'Bill Gates', isCorrect: false},
+//         {answerText: 'Warren Buffett', isCorrect:false},
+//         {answerText: 'Jimmy Page', isCorrect: false},
+//       ],
+//     },
+//     {
+//       questionText: 'Who is Capital of India',
+//       answerOptions:[
+//         {answerText: 'Delhi', isCorrect: true},
+//         {answerText: 'Cairo', isCorrect: false},
+//         {answerText: 'Jakarta', isCorrect:false},
+//         {answerText: 'Mumbai', isCorrect: false},
+//       ],
+//     },
+
+// ];
+
+
+function App() {
+
+  return (
+        <div className="App">
+          <div>
+            <div>Quiz App</div>
+          </div>
+        </div>
+  )
+}
+
+export default App
